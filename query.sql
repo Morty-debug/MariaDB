5a8f2d563718dc6f17400f4d1ea2a7006d5dc273
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-10-28 11:49:26 -0600

    Delete query.sql

--- query.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS pruebadb;
-use pruebadb;
-CREATE TABLE IF NOT EXISTS mensajeria(
-	idmensajeria BIGINT NOT NULL AUTO_INCREMENT,
-	usuarios VARCHAR(100) NOT NULL,
-	mensajes LONGTEXT NOT NULL,
-	fotos BLOB NULL,
-	PRIMARY KEY(idmensajeria)
-);
